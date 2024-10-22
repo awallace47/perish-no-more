71791cdbed1efa7231a273303b7f331d6c832b0e
Andrew Wallace <aswallace92@gmail.com>
2024-10-22 17:28:48 -0500

    basic weapon hit detection

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     public Animator animator;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
 
     public bool IsAttacking { get; private set; }
 
@@ -20,10 +23,35 @@ public class PlayerAttack : MonoBehaviour
     {
         animator.SetTrigger("Attack");
         IsAttacking = true;
+        StartCoroutine(HitDetection());
     }
 
     void AttackAnimCompleted(AnimationEvent evt)
     {
         IsAttacking = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null) return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+    IEnumerator HitDetection()
+    {
+        while (IsAttacking)
+        {
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                Destroy(enemy.gameObject);
+                yield return null;
+            }
+
+            yield return null;
+        }
+
     }
 }
