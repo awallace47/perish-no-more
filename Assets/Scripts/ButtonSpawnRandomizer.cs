5ac2488dd1a8236f9a2b48da0cdf9665471211a9
Andrew Wallace <aswallace92@gmail.com>
2024-10-29 20:02:41 -0500

    background music

---
+++ Assets/Scripts/ButtonSpawnRandomizer.cs
@@ -0,0 +1,9 @@
+﻿
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Button Spawn Randomizer", menuName = "ScriptableObject/ButtonSpawnRandomizer", order = 1)]
+public class ButtonSpawnRandomizer : ScriptableObject
+{
+
+}
