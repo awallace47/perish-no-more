b856aae8ded04599b4ac68839d2de7d831b8026f
Andrew Wallace <aswallace92@gmail.com>
2024-11-08 13:00:31 -0600

    randomize button spawning

--- Assets/Scripts/Spawning/SpawnData.cs
+++ Assets/Scripts/Spawning/SpawnData.cs
@@ -7,4 +7,14 @@ public class SpawnData : ScriptableObject
 {
     public Vector3[] enemyPositions;
     public GameObject[] enemyPrefabs;
+    public GameObject buttonPrefab;
+
+    [Space, Header("Zone 1")]
+    public Vector3[] zone1ButtonPositions;
+
+    [Space, Header("Zone 2")]
+    public Vector3[] zone2ButtonPositions;
+
+    [Space, Header("Zone 3")]
+    public Vector3[] zone3ButtonPositions;
 }
--- Assets/Scripts/Spawning/SpawnRandomizer.cs
+++ Assets/Scripts/Spawning/SpawnRandomizer.cs
@@ -1,15 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnRandomizer : MonoBehaviour
 {
     public SpawnData spawnData;
+    public GameObject zone1Gate;
+    public GameObject zone2Gate;
 
     void Start()
     {
         if (spawnData == null) return;
 
+        SpawnEnemies();
+        SpawnButtons();
+    }
+
+
+    private void SpawnEnemies()
+    {
         int enemyRange = spawnData.enemyPrefabs.Length;
 
         foreach (Vector3 point in spawnData.enemyPositions)
@@ -20,14 +27,25 @@ public class SpawnRandomizer : MonoBehaviour
         }
     }
 
-
-    private void SpawnEnemies()
-    {
-
-    }
-
     private void SpawnButtons()
     {
+        int zone1Range = spawnData.zone1ButtonPositions.Length;
+        int zone2Range = spawnData.zone2ButtonPositions.Length;
+        int zone3Range = spawnData.zone3ButtonPositions.Length;
 
+        Vector3 zone1Position = spawnData.zone1ButtonPositions[Random.Range(0, zone1Range)];
+        Vector3 zone2Position = spawnData.zone2ButtonPositions[Random.Range(0, zone2Range)];
+        Vector3 zone3Position = spawnData.zone3ButtonPositions[Random.Range(0, zone3Range)];
+
+        GameObject zone1Button = Instantiate(spawnData.buttonPrefab);
+        zone1Button.transform.position = zone1Position;
+        zone1Button.GetComponent<ButtonInteraction>().gate = zone1Gate;
+
+        GameObject zone2Button = Instantiate(spawnData.buttonPrefab);
+        zone2Button.transform.position = zone2Position;
+        zone2Button.GetComponent<ButtonInteraction>().gate = zone2Gate;
+
+        GameObject zone3Button = Instantiate(spawnData.buttonPrefab);
+        zone3Button.transform.position = zone3Position;
     }
 }
